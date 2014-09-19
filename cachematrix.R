6a18e373e1262b3c0f41ec68609fe30ac4e48983
vegitle <vegitle@users.noreply.github.com>
2014-09-19 19:55:44 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,24 +1,24 @@
 makeCacheMatrix <- function(x = matrix()) {
-    i <- NULL
-    set <- function(y) {
+    i <- NULL # set i (inverse) to null as no inverse data to begin with
+    set <- function(y) { # stores matrix into variable, when new matrix is created, i will be set back to null as previous inverse will be wrong
         x <<- y
         i <<- NULL
     }
-    get <- function() x 
+    get <- function() x # returns matrix
     setinverse <- function(solve) i <<- solve 
-    getinverse <- function() i
+    getinverse <- function() i # returns inverse
     list(set = set, get = get,
          setinverse = setinverse,
-         getinverse = getinverse)
+         getinverse = getinverse) # list function of 4 variables
 }
 cacheSolve <- function(x, ...) {
-    i <- x$getinverse()
+    i <- x$getinverse() # sets inverse from the getinverse function
     if(!is.null(i)) {
         message("getting cached data")
         return(i)
-    }
-    data <- x$get()
-    i <- solve(data)
-    x$setinverse(i)
+    } # if inverse is not null, display message and show matrix inverse
+    data <- x$get() # if null set matrix into variable
+    i <- solve(data) # inverse the matrix and place in variable
+    x$setinverse(i) # use set inverse variable as i
     i
 }
